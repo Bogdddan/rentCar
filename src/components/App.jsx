5e689fd1637fff84cc6614dee44b47c4fddee711
Bogdan <bogdankrajnik95@gmail.com>
2023-10-11 20:59:29 +0300

    виправлення помилок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
-import {Navigate, Route, Routes} from 'react-router-dom';
-import Main from '../pages/Main/Main';
-import CatalogPage from 'pages/CatalogPage/CatalogPage';
-import Favorites from 'pages/Favorites/Favorites';
-import Layout from './Layout/Layout';
+import { Navigate, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main/Main";
+import CatalogPage from "../pages/CatalogPage/CatalogPage";
+import Favorites from "../pages/Favorites/Favorites";
+import Layout from "./Layout/Layout";
 
-export const App = () => {
+function App() {
   return (
     <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Main/>}/>
-        <Route path="catalog" element={<CatalogPage/>}/>
-        <Route path="favorites" element={<Favorites/>}/>
-        <Route path='*' element={<Navigate to="/" replace/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Main />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<Favorites />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
-     </Routes>
+    </Routes>
   );
-};
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {App} from "./components/App";
+import App from "./components/App";
 import { persistor, store } from "./redux/store";
 import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
